7b19e161c90746078dfeca38ad00e9fa6f6fe33f
Junior <junior.jr.jl@gmail.com>
2026-03-28 11:32:58 -0300

    feat: contract test

---
+++ src/contractTest/resources/contracts/order/orderByIdV1NotFound.groovy
@@ -0,0 +1,22 @@
+package contracts.order
+
+import org.springframework.cloud.contract.spec.Contract
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE
+
+Contract.make {
+    request {
+        method GET()
+        headers {
+            accept APPLICATION_JSON_VALUE
+        }
+        url("/api/v1/orders/01226N0693HDH")
+    }
+    response{
+        status NOT_FOUND()
+        body([
+                instance: fromRequest().path(),
+                type: "/errors/not-found",
+                title: "Not found"
+        ])
+    }
+}
